af912e75e090f112e87190eb9bb69f3afaeaef1b
giudicelli <19559026+giudicelli@users.noreply.github.com>
2020-05-28 21:18:07 +0200

    Using new code from distributed-architecture

--- src/Master/Handlers/ProcessTrait.php
+++ src/Master/Handlers/ProcessTrait.php
@@ -8,14 +8,6 @@ trait ProcessTrait
 {
     protected function buildShellQueueCommand(string $command, ?array $extraParams = null): string
     {
-        if ($this->config->getBinPath()) {
-            $bin = $this->config->getBinPath();
-        } elseif ($this->groupConfig->getBinPath()) {
-            $bin = $this->groupConfig->getBinPath();
-        } else {
-            $bin = PHP_BINARY;
-        }
-
         $params = $this->buildParams();
         $params[HandlerQueue::PARAM_COMMAND] = $command;
         $params[HandlerQueue::PARAM_CONFIG] = $this->config;
@@ -24,8 +16,6 @@ trait ProcessTrait
             $params = array_merge($params, $extraParams);
         }
 
-        $params = escapeshellarg(json_encode($params));
-
-        return $bin.' '.$this->groupConfig->getCommand().' '.$params;
+        return $this->getShellCommand($params);
     }
 }
